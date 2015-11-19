3d61bce79b01431d2c0aad7f569e84b161e69369
Sasikala-Ravichandran <passion.rails@gmail.com>
2015-11-19 14:15:41 -0600

    Finished list and item

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,51 @@
+class TodoItemsController < ApplicationController
+
+    before_action :set_todo_list, only: [ :create, :destroy, :complete]
+    before_action :set_todo_item, except: [:index, :create]
+
+def index
+	@todo_items = TodoItem.all
+end
+ 
+def create
+	@todo_item = @todo_list.todo_items.build(todo_items_params)
+	if @todo_item.save
+       redirect_to @todo_list, notice: "Successfully an item created"
+	else
+       render 'new', error: "Not created an item"
+	end
+
+end
+ 
+def edit
+end
+
+def update
+end
+
+def destroy
+	@todo_item.destroy
+	redirect_to @todo_list
+end
+
+def complete
+   @todo_item.update_attribute(:completed_at, Time.now)
+   redirect_to @todo_list, notice: "Todo item completed"
+end
+
+private
+
+	def todo_items_params
+		params.require(:todo_item).permit(:content)
+	end
+ 
+    def set_todo_list
+    	@todo_list = TodoList.find(params[:todo_list_id])
+    end
+ 
+    def set_todo_item
+    	@todo_item = TodoItem.find(params[:id])
+    end
+
+
+end
